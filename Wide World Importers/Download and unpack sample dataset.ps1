cedf46a7054debd3bd7ffaebf92012e05bcb9a25
Bradley Schacht <bradley.schacht@gmail.com>
2024-04-02 15:44:28 -0400

    Updated Adventure Works folder structure and added Wide World Importers download

---
+++ Wide World Importers/Download and unpack sample dataset.ps1
@@ -0,0 +1,39 @@
+Clear-Host
+
+#Set the parameters for the script.
+[String]$shared_assets_directory    = "C:\GitHub\sample-data\Shared Assets\"
+[String]$data_directory             = "C:\Sample Data\Data\Wide World Importers\"
+[String]$activity_log               = "C:\Sample Data\Activity Log\Wide World Importers\" + (Get-Date -UFormat "%Y_%m_%dT%H_%M_%S") + ".txt"
+
+#List of files to be downloaded in the format of (SaveAsFileName, ProcessedSubdirectory, URL).
+[array]$file_list =
+    @(
+        @{file_name = "WideWorldImporters.bak";                         data_subdirectory = "Database Backups\";            url = "https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Full.bak"},
+        @{file_name = "WideWorldImporters.bacpac";                      data_subdirectory = "Database Backups\";            url = "https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Full.bacpac"},
+        @{file_name = "WideWorldImporters-StandardEdition.bacpac";      data_subdirectory = "Database Backups\";            url = "https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Standard.bacpac"},
+        @{file_name = "WideWorldImportersDW.bak";                       data_subdirectory = "Database Backups\";            url = "https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImportersDW-Full.bak"},
+        @{file_name = "WideWorldImportersDW.bacpac";                    data_subdirectory = "Database Backups\";            url = "https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImportersDW-Full.bacpac"},
+        @{file_name = "WideWorldImportersDW-StandardEdition.bacpac";    data_subdirectory = "Database Backups\";            url = "https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImportersDW-Standard.bacpac"},
+        @{file_name = "Daily.ETL.ispac";                                data_subdirectory = "Resources\";                   url = "https://github.com/microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/Daily.ETL.ispac"},
+        @{file_name = "sample-scripts.zip";                             data_subdirectory = "Resources\sample-scripts";     url = "https://github.com/microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/sample-scripts.zip"},
+        @{file_name = "workload-driver.zip";                            data_subdirectory = "Resources\workload-driver";    url = "https://github.com/microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/workload-drivers.zip"},
+        @{file_name = "Source Code.zip";                                data_subdirectory = "Resources\";                   url = "https://github.com/microsoft/sql-server-samples/archive/refs/tags/wide-world-importers-v1.0.zip"}        
+    )
+
+<#*********************     Do not change any code beyond this point     *********************#>
+
+#Load the Get-SampleData script.
+$script_to_load = "{0}Get-SampleData.ps1" -f $shared_assets_directory
+Invoke-Expression -Command ". `"$script_to_load`""
+Write-Host "Loaded $script_to_load"
+
+$get_sampledata_parameters = @{
+    'shared_assets_directory'   = $shared_assets_directory;
+    'data_directory'            = $data_directory;
+    'activity_log'              = $activity_log;
+    'file_list'                 = $file_list;
+    'download_data'             = $true;
+    'process_downloaded_data'   = $true;
+}
+
+Get-SampleData @get_sampledata_parameters
